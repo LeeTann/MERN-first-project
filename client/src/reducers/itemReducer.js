ce77fdbf413d27b6513deedd26f49b9a4d74561c
ltann <leeroyleeroy18@gmail.com>
2019-03-16 03:18:05 -0700

    Implemented Add item funtionality

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,5 @@
 import uuid from 'uuid'
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEM } from '../actions/types'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types'
 
 const initalState = {
     items: [
@@ -14,13 +14,19 @@ export default function(state = initalState, action) {
     switch(action.type) {
         case GET_ITEMS:
             return{
-                ...state
+                ...state // return a copy of the current state
             }
 
         case DELETE_ITEM:
             return {
                 ...state,
                 items: state.items.filter(item => item.id !== action.payload)
+            }
+        
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
             }
 
         default:
