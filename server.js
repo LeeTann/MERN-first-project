c58f2d8fb39c7eb332c8a50ea2a0a277696ff936
ltann <leeroyleeroy18@gmail.com>
2020-03-23 21:22:05 -0700

    Adjust mongo connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = require('./config/keys').mongoURI
 
 // Connect to Mongo
 mongoose
-    .connect(db)
+    .connect(db, {useCreateIndex: true})
     .then(() => {console.log('MongoDB connected...')})
     .catch(err => console.log(err))
 
